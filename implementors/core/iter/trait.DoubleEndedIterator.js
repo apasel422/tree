e9c52e2bc138c79f7659fbba0aec8422377319db
Andrew Paseltiner <apaseltiner@gmail.com>
2015-03-05 14:24:56 -0500

    rebuild pages at 779ef5c

---
+++ implementors/core/iter/trait.DoubleEndedIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['tree'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a> for <a class='struct' href='tree/struct.IntoIter.html' title='tree::IntoIter'>IntoIter</a>&lt;K, V&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'a, K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a> for <a class='struct' href='tree/struct.Iter.html' title='tree::Iter'>Iter</a>&lt;'a, K, V&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'a, K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a> for <a class='struct' href='tree/struct.IterMut.html' title='tree::IterMut'>IterMut</a>&lt;'a, K, V&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
