a3c2f17ed24776e4dadfa1c944eb3fbf57dca4cb
Andrew Paseltiner <apaseltiner@gmail.com>
2015-03-13 11:05:56 +0000

    rebuild pages at 941d9b3

--- tree/sidebar-items.js
+++ tree/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["map",""],["set",""]]});
+initSidebarItems({"mod":[["map","An ordered map based on a binary search tree."],["set","An ordered set based on a binary search tree."]]});
