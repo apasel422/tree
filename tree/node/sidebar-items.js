30e4af8c707d1a6f5fc160870a5d741b3615a651
Andrew Paseltiner <apaseltiner@gmail.com>
2015-04-01 17:37:58 +0000

    rebuild pages at 9adb72c

--- tree/node/sidebar-items.js
+++ tree/node/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["Extreme",""],["Traverse",""]],"struct":[["OccupiedEntry","An occupied entry."],["VacantEntry","A vacant entry."]],"mod":[["build",""]]});
+initSidebarItems({"trait":[["Extreme",""],["Traverse",""]],"mod":[["build",""]],"struct":[["OccupiedEntry","An occupied entry."],["VacantEntry","A vacant entry."]]});
