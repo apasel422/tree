4a2973d21050b79aec4f521116ecf875669e5f53
Andrew Paseltiner <apaseltiner@gmail.com>
2015-04-17 17:22:29 +0000

    rebuild pages at a69408c

--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['tree'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;K, V, C&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='tree/map/struct.Map.html' title='tree::map::Map'>Map</a>&lt;K, V, C&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;T, C&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='tree/set/struct.Set.html' title='tree::set::Set'>Set</a>&lt;T, C&gt;",];
+implementors['tree'] = ["impl&lt;K, V, C&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='tree/map/struct.Map.html' title='tree::map::Map'>Map</a>&lt;K, V, C&gt;","impl&lt;T, C&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='tree/set/struct.Set.html' title='tree::set::Set'>Set</a>&lt;T, C&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/ordered_iter/trait.OrderedSetIterator.js
+++ implementors/ordered_iter/trait.OrderedSetIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['tree'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;T&gt; OrderedSetIterator for <a class='struct' href='tree/set/struct.IntoIter.html' title='tree::set::IntoIter'>IntoIter</a>&lt;T&gt;","<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, T&gt; OrderedSetIterator for <a class='struct' href='tree/set/struct.Iter.html' title='tree::set::Iter'>Iter</a>&lt;'a, T&gt;",];
+implementors['tree'] = ["impl&lt;T&gt; OrderedSetIterator for <a class='struct' href='tree/set/struct.IntoIter.html' title='tree::set::IntoIter'>IntoIter</a>&lt;T&gt;","impl&lt;'a, T&gt; OrderedSetIterator for <a class='struct' href='tree/set/struct.Iter.html' title='tree::set::Iter'>Iter</a>&lt;'a, T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
