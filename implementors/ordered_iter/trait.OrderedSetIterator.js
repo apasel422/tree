edade20378c4b3aa0c56b5337556d871fd0639cf
Andrew Paseltiner <apaseltiner@gmail.com>
2015-10-07 14:37:32 +0000

    rebuild pages at feb8808

--- implementors/ordered_iter/trait.OrderedSetIterator.js
+++ implementors/ordered_iter/trait.OrderedSetIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['tree'] = ["impl&lt;T&gt; OrderedSetIterator for <a class='struct' href='tree/set/struct.IntoIter.html' title='tree::set::IntoIter'>IntoIter</a>&lt;T&gt;","impl&lt;'a, T&gt; OrderedSetIterator for <a class='struct' href='tree/set/struct.Iter.html' title='tree::set::Iter'>Iter</a>&lt;'a, T&gt;",];
+implementors['tree'] = ["impl&lt;T&gt; OrderedSetIterator for <a class='struct' href='tree/set/struct.IntoIter.html' title='tree::set::IntoIter'>IntoIter</a>&lt;T&gt; <span class='where'>where T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a></span>","impl&lt;'a, T&gt; OrderedSetIterator for <a class='struct' href='tree/set/struct.Iter.html' title='tree::set::Iter'>Iter</a>&lt;'a, T&gt; <span class='where'>where T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a></span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
